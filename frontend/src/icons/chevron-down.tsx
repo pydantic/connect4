8a8c4ac216017a561a54ae245e87c3366aec68fc
Daniel Cruz <20308945+ddanielcruzz@users.noreply.github.com>
2025-05-14 10:17:32 -0600

    feat: use more complex select component (#5)

---
+++ frontend/src/icons/chevron-down.tsx
@@ -0,0 +1,20 @@
+import { Component } from 'solid-js'
+
+export const ChevronDown: Component<{ class?: string }> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class={props.class}
+    >
+      <path d="m6 9 6 6 6-6" />
+    </svg>
+  )
+}
