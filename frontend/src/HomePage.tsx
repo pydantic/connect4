6b0202d782b49e0f73981a21d8e671f52b80d60c
Samuel Colvin <s@muelcolvin.com>
2025-05-15 06:27:09 -0400

    set URLs for traces and prize draw

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -10,10 +10,15 @@ const HomePage: Component = () => {
         <A href="/connect4" class={styles.button}>
           Play the game - Connect 4
         </A>
-        <A href="https://example.com" target="_blank" rel="noopener noreferrer" class={styles.button}>
+        <A
+          href="https://logfire-us.pydantic.dev/l/join-samuelcolvin/VKJ1XdXRvo"
+          target="_blank"
+          rel="noopener noreferrer"
+          class={styles.button}
+        >
           See game traces in Logfire
         </A>
-        <A href="#" class={styles.button}>
+        <A href="https://forms.gle/akrTxE3yxqFduxdN7" class={styles.button}>
           Join the Prize Draw
         </A>
       </div>
