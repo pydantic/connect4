048f9b58af84e36cdd7512ae0265f7c39694deab
Daniel Cruz <20308945+ddanielcruzz@users.noreply.github.com>
2025-05-15 10:10:26 -0600

    feat: fix scrolling issue and add footer (#10)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,20 @@ import HomePage from './HomePage'
 import PlayerSelection from './PlayerSelection'
 import HumanAIPlay from './HumanAIPlay'
 import AiVsAiPlay from './AivsAiPlay'
+import { Layout } from './layout'
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <Router>
-        <Route path="/" component={HomePage} />
-        <Route path="/connect4" component={PlayerSelection} />
-        <Route path="/connect4/human-vs-ai/:gameId" component={HumanAIPlay} />
-        <Route path="/connect4/ai-vs-ai/:gameId" component={AiVsAiPlay} />
-      </Router>
-    </div>
+    <Layout>
+      <div class={styles.App}>
+        <Router>
+          <Route path="/" component={HomePage} />
+          <Route path="/connect4" component={PlayerSelection} />
+          <Route path="/connect4/human-vs-ai/:gameId" component={HumanAIPlay} />
+          <Route path="/connect4/ai-vs-ai/:gameId" component={AiVsAiPlay} />
+        </Router>
+      </div>
+    </Layout>
   )
 }
 
