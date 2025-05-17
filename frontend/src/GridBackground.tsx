f6efacb95e2be5ff10feceae672e2b5c8c4bb837
Laura Summers <summerscope@gmail.com>
2025-05-17 09:40:26 +0200

    Redesign views and homepage

---
+++ frontend/src/GridBackground.tsx
@@ -0,0 +1,20 @@
+import { Component, JSX } from 'solid-js';
+import styles from './GridBackground.module.css';
+
+interface GridBackgroundProps {
+  children: JSX.Element;
+}
+
+const GridBackground: Component<GridBackgroundProps> = (props) => {
+  return (
+    <div class={styles.gridContainer}>
+      <div class={styles.gridLines}>
+        <div></div> {/* Top horizontal line */}
+        <div></div> {/* Bottom horizontal line */}
+      </div>
+      {props.children}
+    </div>
+  );
+};
+
+export default GridBackground; 
